a06fdc7a28ddb6c03f32e8d56ec30c2a2fa4d45e
MrSavvy98 <pamuditha.rasanjana99@gmail.com>
2020-10-17 10:59:56 +0530

    initial commit

---
+++ ITP/NodeJS/Models/customerPayments.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+var customerPayemnt = mongoose.model('customerPayemnt', {
+    customerId: { type: String },
+    customerName: { type: String },
+    orderDate: { type: Date },
+    dueDate: { type: Date },
+    amount: { type:Number},
+    paidAmount:{ type:Number},
+    DueAmont:{type: Number}
+});
+
+module.exports = { customerPayemnt };
